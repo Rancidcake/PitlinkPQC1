28d59aa8cb7af8c9e9e8969a2585a780d6ffc8db
Mayank Hete <mayankhete@Mayanks-MacBook-Pro.local>
2025-11-15 17:47:05 +0530

    Add csv_lz4_tool: small LZ4 CSV compressor/decompressor

---
+++ csv_lz4_tool/target/release/deps/mytool-c907845d069ec113.d
@@ -0,0 +1,5 @@
+/Users/mayankhete/Desktop/PitlinkPQC/csv_lz4_tool/target/release/deps/mytool-c907845d069ec113.d: src/main.rs
+
+/Users/mayankhete/Desktop/PitlinkPQC/csv_lz4_tool/target/release/deps/mytool-c907845d069ec113: src/main.rs
+
+src/main.rs:
---
+++ csv_lz4_tool/target/release/mytool.d
@@ -0,0 +1,1 @@
+/Users/mayankhete/Desktop/PitlinkPQC/csv_lz4_tool/target/release/mytool: /Users/mayankhete/Desktop/PitlinkPQC/csv_lz4_tool/src/main.rs
